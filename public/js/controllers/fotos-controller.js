4fa46996ce09dbe5408ec32cce8c0b5063385902
Lucas Reis <lucas.reis@strategymanager.com.br>
2016-11-23 18:01:07 -0200

    adicionado o module e o controller da nossa aplicação

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,7 @@
+angular.module('alurapic').controller('FotosController', function($scope) {
+
+    $scope.foto = {
+        url : "http://41.media.tumblr.com/658688f96c218a092c2856bd39291c99/tumblr_nzbiuikY7u1u4jtvbo1_500.jpg",
+        titulo : "GameCube Logo"
+    };
+});
